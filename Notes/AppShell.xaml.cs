a1de791a6a3e2c4b8b44123ec54507217e039deb
willianmz <willianmazzorana@hotmail.com>
2023-06-10 06:58:48 -0300

    Finalizado tutorial

---
+++ Notes/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Notes;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+
+        //registra a rota de navegacao
+        //O primeiro parâmetro é o nome da cadeia de caracteres do URI que você deseja registrar, nesse caso, o nome resolvido é "NotePage".
+        //O segundo parâmetro é o tipo de página a ser carregada quando "NotePage" é navegada.
+        Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+    }
+}
